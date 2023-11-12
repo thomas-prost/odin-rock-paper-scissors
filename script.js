a63d99f629eec9fda49da43d715c5eb18c7e2a37
Thomas Prost <mail@thprost.eu>
2023-11-12 11:48:55 +0100

    HTML background changed. JS script returns both computer and user choices.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hellow world");
+// ask computer what it selects
+// ask user what they select
+// transform user select in lowercase
+// IF compare computer and user select for computer win
+//   display you loose
+// IFELSE compare computer and user select for user win
+//   display you win
+// ELSE compare computer and user select for equality
+//   display equality
+// re-run
+
+let getComputerChoice = ['rock', 'paper', 'scissors']; 
+let randomComputerChoice = Math.floor(Math.random() * getComputerChoice.length);
+console.log(getComputerChoice[randomComputerChoice]);
+
+let getUserChoice = prompt("Rock, Paper or Scissors?");
+let cleanUserChoice = getUserChoice.toLowerCase();
+console.log(cleanUserChoice);
