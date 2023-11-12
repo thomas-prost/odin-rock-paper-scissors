6543f14d7ddd0aee60687645e24b5014b0eff6d9
Thomas Prost <mail@thprost.eu>
2023-11-12 13:59:49 +0100

    Game and rematch implementation.

--- script.js
+++ script.js
@@ -9,11 +9,73 @@
 //   display equality
 // re-run
 
-let getComputerChoice = ['rock', 'paper', 'scissors']; 
-let randomComputerChoice = Math.floor(Math.random() * getComputerChoice.length);
-console.log(getComputerChoice[randomComputerChoice]);
+function game() {
 
-let getUserChoice = prompt("Rock, Paper or Scissors?");
-let cleanUserChoice = getUserChoice.toLowerCase();
-console.log(cleanUserChoice);
+    let roundCounter = 1;
+    let playerScore = 0;
+    let computerScore = 0;
 
+    console.log("!! Match " + matchCounter + " !!")
+
+    for (let i = 5; i >= roundCounter; roundCounter++) {
+
+        function compareChoices() {
+            
+            let getComputerChoice = ['rock', 'paper', 'scissors']; 
+            let randomComputerChoice = String(Math.floor(Math.random() * getComputerChoice.length));
+            let computerSelection = getComputerChoice[randomComputerChoice];
+            
+            let getUserChoice = prompt("Rock, Paper or Scissors?") || "";
+            let playerSelection = getUserChoice.toLowerCase();
+            
+            if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+                alert('Your choice "' + playerSelection + '" does not exist');
+                alert("This round doesn't count.");
+                roundCounter -= 1;
+            }
+            else if (computerSelection == playerSelection) {
+                alert("Equality! You both chose " + computerSelection);
+            }
+            else if (computerSelection == "rock" && playerSelection == "scissors" ||
+                     computerSelection == "paper" && playerSelection == "rock" ||
+                     computerSelection == "scissors" && playerSelection == "paper") {
+                computerScore += 1 ;
+                alert("You lost... " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + "...");
+            }
+            else {
+                alert("You won! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + "!");
+                playerScore += 1;
+            }
+        }
+        compareChoices();
+        console.log("Round "+ roundCounter + " — Player: "+ playerScore + " points — Computer: " + computerScore + " points.");
+    }
+
+    if (computerScore > playerScore) {
+        console.log("The computer won " + computerScore + " to " + playerScore)
+        alert("Unfortunately, you lost this match " + playerScore + " to " + computerScore + "...")
+    }
+    else if (playerScore > computerScore) {
+        console.log("You won " + playerScore + " to " + computerScore)
+        alert("Congratulations! You won this match " + playerScore + " to " + computerScore + "!")
+    }
+    else {
+        console.log("Equality " + computerScore + " to " + playerScore)
+        alert("This match ended with an equality: " + playerScore + " to " + computerScore + ".")
+    }
+}
+
+let rematch = true;
+let matchCounter = 1;
+game();
+
+while (rematch) {
+    if (confirm("Do you want a rematch?") == true) {
+        matchCounter++ ;
+        game();
+    }
+    else {
+        alert("Thank you for playing!");
+        rematch = false;
+    }
+}
