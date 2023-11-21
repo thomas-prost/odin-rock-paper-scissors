d7ea024f32c820394d8d8b25bf988fc967047b46
Thomas Prost <mail@thprost.eu>
2023-11-21 17:51:23 +0100

    Replaces console UI by GUI.

--- script.js
+++ script.js
@@ -1,81 +1,129 @@
-// ask computer what it selects
-// ask user what they select
-// transform user select in lowercase
-// IF compare computer and user select for computer win
-//   display you loose
-// IFELSE compare computer and user select for user win
-//   display you win
-// ELSE compare computer and user select for equality
-//   display equality
-// re-run
+// function game() {
 
-function game() {
+let roundNumber = 1;
+let matchNumber = 1;
+let playerSelection = "";
+let playerScore = 0;
+let computerScore = 0;
 
-    let roundCounter = 1;
-    let playerScore = 0;
-    let computerScore = 0;
+const matchCounter = document.querySelector(".matchCounter");
+const roundCounter = document.querySelector(".roundCounter");
+const playerScoreCounter = document.querySelector(".playerScoreCounter");
+const computerScoreCounter = document.querySelector(".computerScoreCounter");
 
-    console.log("!! Match " + matchCounter + " !!")
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-    for (let i = 5; i >= roundCounter; roundCounter++) {
+rock.addEventListener("click", () => {
+    playerSelection = "rock";
+    playRound();
+});
+paper.addEventListener("click", () => {
+    playerSelection = "paper";
+    playRound();
+});
+scissors.addEventListener("click", () => {
+    playerSelection = "scissors";
+    playRound();
+});
 
-        function compareChoices() {
-            
-            let getComputerChoice = ['rock', 'paper', 'scissors']; 
-            let randomComputerChoice = String(Math.floor(Math.random() * getComputerChoice.length));
-            let computerSelection = getComputerChoice[randomComputerChoice];
-            
-            let getUserChoice = prompt("Rock, Paper or Scissors?") || "";
-            let playerSelection = getUserChoice.toLowerCase();
-            
-            if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-                alert('Your choice "' + playerSelection + '" does not exist');
-                alert("This round doesn't count.");
-                roundCounter -= 1;
-            }
-            else if (computerSelection == playerSelection) {
-                alert("Equality! You both chose " + computerSelection);
-            }
-            else if (computerSelection == "rock" && playerSelection == "scissors" ||
-                     computerSelection == "paper" && playerSelection == "rock" ||
-                     computerSelection == "scissors" && playerSelection == "paper") {
-                computerScore += 1 ;
-                alert("You lost... " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + "...");
-            }
-            else {
-                alert("You won! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + "!");
-                playerScore += 1;
-            }
-        }
-        compareChoices();
-        console.log("Round "+ roundCounter + " — Player: "+ playerScore + " points — Computer: " + computerScore + " points.");
+function updateScores() {
+    matchCounter.textContent = "Match " + matchNumber;
+    roundCounter.textContent = "Round " + roundNumber;
+    playerScoreCounter.textContent = "You have " + playerScore + " points.";
+    computerScoreCounter.textContent = "The computer has " + computerScore + " points.";
+}
+
+updateScores();
+
+// for (let i = 5; i >= roundCounter; roundCounter++) {
+
+function playRound() {
+    
+    const roundResult = document.querySelector(".roundResult");
+    const roundPara = document.querySelector(".roundPara");
+    
+    const matchResult = document.querySelector(".matchResult");
+    const matchPara = document.querySelector(".matchPara");
+    
+    let getComputerChoice = ['rock', 'paper', 'scissors']; 
+    let randomComputerChoice = String(Math.floor(Math.random() * getComputerChoice.length));
+    let computerSelection = getComputerChoice[randomComputerChoice];
+    
+    if (computerSelection == playerSelection) {
+        roundResult.style.cssText = "background-color: grey; display: block;";
+        roundPara.textContent = "Equality! You both chose " + computerSelection;
+        roundNumber += 1;
     }
-
-    if (computerScore > playerScore) {
-        console.log("The computer won " + computerScore + " to " + playerScore)
-        alert("Unfortunately, you lost this match " + playerScore + " to " + computerScore + "...")
-    }
-    else if (playerScore > computerScore) {
-        console.log("You won " + playerScore + " to " + computerScore)
-        alert("Congratulations! You won this match " + playerScore + " to " + computerScore + "!")
+    else if (computerSelection == "rock" && playerSelection == "scissors" ||
+    computerSelection == "paper" && playerSelection == "rock" ||
+    computerSelection == "scissors" && playerSelection == "paper") {
+        computerScore += 1 ;
+        roundResult.style.cssText = "background-color: brown; display: block;";
+        roundPara.textContent = "You lost... " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + "...";
+        roundNumber += 1;
     }
     else {
-        console.log("Equality " + computerScore + " to " + playerScore)
-        alert("This match ended with an equality: " + playerScore + " to " + computerScore + ".")
+        roundResult.style.cssText = "background-color: green; display: block;";
+        roundPara.textContent = "You won! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + "!";
+        playerScore += 1;
+        roundNumber += 1;
+    }
+    updateScores();
+    
+    if (playerScore == 5) {
+        matchResult.style.cssText = "background-color: green; display: block;";
+        matchPara.textContent = "Match " + matchNumber + ": You won " + playerScore + " to " + computerScore + "!";
+        playerScore = 0;
+        computerScore = 0;
+        roundNumber = 1
+        matchNumber += 1;
+    } else if (computerScore == 5) {
+        matchResult.style.cssText = "background-color: brown; display: block;";
+        matchPara.textContent = "Match " + matchNumber + ": You lost " + playerScore + " to " + computerScore + "...";
+        playerScore = 0;
+        computerScore = 0;
+        roundNumber = 1;
+        matchNumber += 1;
     }
 }
 
-let rematch = true;
-let matchCounter = 1;
-game();
 
-while (rematch) {
-    if (confirm("Do you want a rematch?") == true) {
-        matchCounter++ ;
-        game();
-    }
-    else {
-        alert("Thank you for playing!");
-        rematch = false;
-    }
-}
+
+
+
+
+
+
+// playRound();
+// console.log("Round "+ roundCounter + " — Player: "+ playerScore + " points — Computer: " + computerScore + " points.");
+
+
+// if (computerScore > playerScore) {
+//     console.log("The computer won " + computerScore + " to " + playerScore)
+//     alert("Unfortunately, you lost this match " + playerScore + " to " + computerScore + "...")
+// }
+// else if (playerScore > computerScore) {
+//     console.log("You won " + playerScore + " to " + computerScore)
+//     alert("Congratulations! You won this match " + playerScore + " to " + computerScore + "!")
+// }
+// else {
+//     console.log("Equality " + computerScore + " to " + playerScore)
+//     alert("This match ended with an equality: " + playerScore + " to " + computerScore + ".")
+// }
+
+
+// let rematch = true;
+// game();
+
+// while (rematch) {
+//     if (confirm("Do you want a rematch?") == true) {
+//         matchCounter++ ;
+//         game();
+//     }
+//     else {
+//         alert("Thank you for playing!");
+//         rematch = false;
+//     }
+// }
